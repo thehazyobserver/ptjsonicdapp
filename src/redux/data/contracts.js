750fc6c37eb1cb37b7067a24893a45a19116bc0c
thehazyobserver <114879476+thehazyobserver@users.noreply.github.com>
2024-12-29 20:53:43 -0500

    Update contracts.js

--- src/redux/data/contracts.js
+++ src/redux/data/contracts.js
@@ -1,7 +1,7 @@
 // src/redux/data/contracts.js
 import erc721Abi from '../blockchain/abis/erc721Abi.json'; // Ensure the ABI file is correctly imported
 
-export const LootBoxNFT = {
+export const PassTheJointContract = {
   abi: erc721Abi,
   address: "", // Address will be set dynamically
 };
